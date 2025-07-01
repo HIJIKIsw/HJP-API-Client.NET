e8302abdb24d670b8b6b82917ee8c404a8bc5d34
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2025-07-02 01:50:45 +0900

    メッセージクラス追加

---
+++ HJP-API-Client.NET/Common/Messages.cs
@@ -0,0 +1,10 @@
+namespace Hjp.Api.Client.Common
+{
+    public static class Messages
+    {
+        public static class Erros
+        {
+            public const string EmptyResponseBody = "Empty response body";
+        }
+    }
+}
