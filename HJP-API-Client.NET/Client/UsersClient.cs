76e3bde6c1f6753d87682ba498295382db47acb6
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2025-07-02 00:28:16 +0900

    ログインAPIが正しく実行できていなかったのを修正

--- HJP-API-Client.NET/Client/UsersClient.cs
+++ HJP-API-Client.NET/Client/UsersClient.cs
@@ -2,7 +2,7 @@ using System.Net.Http.Json;
 using System.Text.Json;
 using Hjp.Api.Client.Dto;
 using Hjp.Api.Client.Utilities;
-using Hjp.Shared.Dto.Users.Me;
+using Hjp.Shared.Dto.Users.Login;
 
 namespace Hjp.Api.Client
 {
@@ -15,28 +15,29 @@ namespace Hjp.Api.Client
             this.httpClient = httpClient;
         }
 
-        public async Task<ApiResponse<UserResponse>> LoginAsync(string accessToken, CancellationToken cancellationToken = default)
+        public async Task<ApiResponse<UserLoginResponse>> LoginAsync(string accessToken, CancellationToken cancellationToken = default)
         {
+            var jsonOptions = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
             var postData = new
             {
                 AccessToken = accessToken
             };
-            var postBody = JsonSerializer.Serialize(postData);
-            using var content = new StringContent(postBody, System.Text.Encoding.UTF8, "application/json");
-            var responseRaw = await this.httpClient.PostAsync("/users/login", content);
-            if (responseRaw.IsSuccessStatusCode == false)
+            var response = await this.httpClient.PostAsJsonAsync(
+                "/users/login",
+                postData,
+                jsonOptions,
+                cancellationToken);
+            if (response.IsSuccessStatusCode == false)
             {
-                return ResponseUtility.CreateErrorResponse<UserResponse>(responseRaw.StatusCode, await responseRaw.Content.ReadAsStringAsync());
+                return ResponseUtility.CreateErrorResponse<UserLoginResponse>(response.StatusCode, await response.Content.ReadAsStringAsync());
             }
-            var responseStream = await responseRaw.Content.ReadAsStreamAsync();
-            var jsonOptions = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
-            var result = await responseRaw.Content.ReadFromJsonAsync<UserResponse>(jsonOptions, cancellationToken);
+            var result = await response.Content.ReadFromJsonAsync<UserLoginResponse>(jsonOptions, cancellationToken);
             if (result == null)
             {
                 throw new InvalidOperationException("Empty response body");
             }
 
-            return ResponseUtility.CreateSuccessResponse<UserResponse>(responseRaw.StatusCode, result);
+            return ResponseUtility.CreateSuccessResponse<UserLoginResponse>(response.StatusCode, result);
         }
     }
 }
--- HJP-API-Client.NET/Interfaces/IUsersClient.cs
+++ HJP-API-Client.NET/Interfaces/IUsersClient.cs
@@ -1,10 +1,10 @@
 using Hjp.Api.Client.Dto;
-using Hjp.Shared.Dto.Users.Me;
+using Hjp.Shared.Dto.Users.Login;
 
 namespace Hjp.Api.Client
 {
     public interface IUsersClient
     {
-        Task<ApiResponse<UserResponse>> LoginAsync(string accessToken, CancellationToken cancellationToken = default);
+        Task<ApiResponse<UserLoginResponse>> LoginAsync(string accessToken, CancellationToken cancellationToken = default);
     }
 }
