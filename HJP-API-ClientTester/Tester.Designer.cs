16eb903f248b10d8bcb1cf355752d0d5e9c2dffa
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2025-07-03 19:02:56 +0900

    UsersControllerのテスターを実装

--- HJP-API-ClientTester/Tester.Designer.cs
+++ HJP-API-ClientTester/Tester.Designer.cs
@@ -31,6 +31,12 @@
             flowLayoutPanel1 = new FlowLayoutPanel();
             loginButton = new Button();
             getProfileButton = new Button();
+            getBalanceButton = new Button();
+            getTransactionsButton = new Button();
+            getStatsButton = new Button();
+            depositButton = new Button();
+            withdrawButton = new Button();
+            transferButton = new Button();
             flowLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
@@ -40,6 +46,12 @@
             flowLayoutPanel1.AutoScroll = true;
             flowLayoutPanel1.Controls.Add(loginButton);
             flowLayoutPanel1.Controls.Add(getProfileButton);
+            flowLayoutPanel1.Controls.Add(getBalanceButton);
+            flowLayoutPanel1.Controls.Add(getTransactionsButton);
+            flowLayoutPanel1.Controls.Add(getStatsButton);
+            flowLayoutPanel1.Controls.Add(depositButton);
+            flowLayoutPanel1.Controls.Add(withdrawButton);
+            flowLayoutPanel1.Controls.Add(transferButton);
             flowLayoutPanel1.Location = new Point(12, 12);
             flowLayoutPanel1.Name = "flowLayoutPanel1";
             flowLayoutPanel1.Size = new Size(592, 466);
@@ -69,13 +81,85 @@
             getProfileButton.UseVisualStyleBackColor = true;
             getProfileButton.Click += getProfileButton_Click;
             // 
-            // Form1
+            // getBalanceButton
+            // 
+            getBalanceButton.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            getBalanceButton.AutoSize = true;
+            getBalanceButton.Location = new Point(3, 69);
+            getBalanceButton.Name = "getBalanceButton";
+            getBalanceButton.Size = new Size(556, 27);
+            getBalanceButton.TabIndex = 29;
+            getBalanceButton.Text = "users/balance";
+            getBalanceButton.UseVisualStyleBackColor = true;
+            getBalanceButton.Click += getBalanceButton_Click;
+            // 
+            // getTransactionsButton
+            // 
+            getTransactionsButton.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            getTransactionsButton.AutoSize = true;
+            getTransactionsButton.Location = new Point(3, 102);
+            getTransactionsButton.Name = "getTransactionsButton";
+            getTransactionsButton.Size = new Size(556, 27);
+            getTransactionsButton.TabIndex = 30;
+            getTransactionsButton.Text = "users/transactions";
+            getTransactionsButton.UseVisualStyleBackColor = true;
+            getTransactionsButton.Click += getTransactionsButton_Click;
+            // 
+            // getStatsButton
+            // 
+            getStatsButton.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            getStatsButton.AutoSize = true;
+            getStatsButton.Location = new Point(3, 135);
+            getStatsButton.Name = "getStatsButton";
+            getStatsButton.Size = new Size(556, 27);
+            getStatsButton.TabIndex = 31;
+            getStatsButton.Text = "users/stats";
+            getStatsButton.UseVisualStyleBackColor = true;
+            getStatsButton.Click += getStatsButton_Click;
+            // 
+            // depositButton
+            // 
+            depositButton.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            depositButton.AutoSize = true;
+            depositButton.Location = new Point(3, 168);
+            depositButton.Name = "depositButton";
+            depositButton.Size = new Size(556, 27);
+            depositButton.TabIndex = 32;
+            depositButton.Text = "users/deposit";
+            depositButton.UseVisualStyleBackColor = true;
+            depositButton.Click += depositButton_Click;
+            // 
+            // withdrawButton
+            // 
+            withdrawButton.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            withdrawButton.AutoSize = true;
+            withdrawButton.Location = new Point(3, 201);
+            withdrawButton.Name = "withdrawButton";
+            withdrawButton.Size = new Size(556, 27);
+            withdrawButton.TabIndex = 33;
+            withdrawButton.Text = "users/withdraw";
+            withdrawButton.UseVisualStyleBackColor = true;
+            withdrawButton.Click += withdrawButton_Click;
+            // 
+            // transferButton
+            // 
+            transferButton.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            transferButton.AutoSize = true;
+            transferButton.Location = new Point(3, 234);
+            transferButton.Name = "transferButton";
+            transferButton.Size = new Size(556, 27);
+            transferButton.TabIndex = 34;
+            transferButton.Text = "users/transfer";
+            transferButton.UseVisualStyleBackColor = true;
+            transferButton.Click += transferButton_Click;
+            // 
+            // Tester
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(616, 490);
             Controls.Add(flowLayoutPanel1);
-            Name = "Form1";
+            Name = "Tester";
             Text = "Form1";
             flowLayoutPanel1.ResumeLayout(false);
             flowLayoutPanel1.PerformLayout();
@@ -87,5 +171,11 @@
         private Button loginButton;
         private FlowLayoutPanel flowLayoutPanel1;
         private Button getProfileButton;
+        private Button getBalanceButton;
+        private Button getTransactionsButton;
+        private Button getStatsButton;
+        private Button depositButton;
+        private Button withdrawButton;
+        private Button transferButton;
     }
 }
--- HJP-API-ClientTester/Tester.cs
+++ HJP-API-ClientTester/Tester.cs
@@ -1,6 +1,11 @@
 using Hjp.Api.Client;
 using Hjp.Api.Client.Tester;
+using Hjp.Shared.Dto.Users.Deposit;
+using Hjp.Shared.Dto.Users.Transfer;
+using Hjp.Shared.Dto.Users.Withdraw;
+using Hjp.Shared.Entities;
 using System.Diagnostics;
+using System.Text.Json;
 
 namespace HJP_API_ClientTester
 {
@@ -17,15 +22,12 @@ namespace HJP_API_ClientTester
 
         private async void loginButton_Click(object sender, EventArgs e)
         {
-            this.loginButton.Enabled = false;
+            var button = (Button)sender;
+            button.Enabled = false;
             try
             {
                 var result = await this.hjpApiClient.LoginWithUserAsync(AppSettings.AccessToken);
-                if (result.IsSuccess == false)
-                {
-                    throw new Exception(result.Message);
-                }
-                Debug.WriteLine("Login Success: " + result.Result?.UserName);
+                Debug.WriteLine($"{button.Name}: " + JsonSerializer.Serialize(result));
             }
             catch (Exception ex)
             {
@@ -33,21 +35,18 @@ namespace HJP_API_ClientTester
             }
             finally
             {
-                this.loginButton.Enabled = true;
+                button.Enabled = true;
             }
         }
 
         private async void getProfileButton_Click(object sender, EventArgs e)
         {
-            this.getProfileButton.Enabled = false;
+            var button = (Button)sender;
+            button.Enabled = false;
             try
             {
                 var result = await this.hjpApiClient.UsersClient.GetProfileAsync();
-                if (result.IsSuccess == false)
-                {
-                    throw new Exception(result.Message);
-                }
-                Debug.WriteLine("GetProfile Success: " + result.Result?.Name);
+                Debug.WriteLine($"{button.Name}: " + JsonSerializer.Serialize(result));
             }
             catch (Exception ex)
             {
@@ -55,7 +54,137 @@ namespace HJP_API_ClientTester
             }
             finally
             {
-                this.getProfileButton.Enabled = true;
+                button.Enabled = true;
+            }
+        }
+
+        private async void getBalanceButton_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            button.Enabled = false;
+            try
+            {
+                var result = await this.hjpApiClient.UsersClient.GetBalanceAsync();
+                Debug.WriteLine($"{button.Name}: " + JsonSerializer.Serialize(result));
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                button.Enabled = true;
+            }
+        }
+
+        private async void getTransactionsButton_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            button.Enabled = false;
+            try
+            {
+                var result = await this.hjpApiClient.UsersClient.GetTransactionsAsync();
+                Debug.WriteLine($"{button.Name}: " + JsonSerializer.Serialize(result));
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                button.Enabled = true;
+            }
+        }
+
+        private async void getStatsButton_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            button.Enabled = false;
+            try
+            {
+                var result = await this.hjpApiClient.UsersClient.GetStatsAsync();
+                Debug.WriteLine($"{button.Name}: " + JsonSerializer.Serialize(result));
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                button.Enabled = true;
+            }
+        }
+
+        private async void depositButton_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            button.Enabled = false;
+            try
+            {
+                var request = new UserDepositRequest()
+                {
+                    Amount = 10,
+                    Description = "TestByTester: " + DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString(),
+                };
+                var result = await this.hjpApiClient.UsersClient.DepositAsync(request);
+                Debug.WriteLine($"{button.Name}: " + JsonSerializer.Serialize(result));
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                button.Enabled = true;
+            }
+        }
+
+        private async void withdrawButton_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            button.Enabled = false;
+            try
+            {
+                var request = new UserWithdrawRequest()
+                {
+                    Amount = 15,
+                    Description = "TestByTester: " + DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString(),
+                };
+                var result = await this.hjpApiClient.UsersClient.WithdrawAsync(request);
+                Debug.WriteLine($"{button.Name}: " + JsonSerializer.Serialize(result));
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                button.Enabled = true;
+            }
+        }
+
+        private async void transferButton_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            button.Enabled = false;
+            try
+            {
+                var request = new UserTransferRequest()
+                {
+                    ToDiscordUserId = 555,
+                    Amount = 5,
+                    Description = "TestByTester: " + DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString(),
+                };
+                var result = await this.hjpApiClient.UsersClient.TransferAsync(request);
+                Debug.WriteLine($"{button.Name}: " + JsonSerializer.Serialize(result));
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                button.Enabled = true;
             }
         }
     }
