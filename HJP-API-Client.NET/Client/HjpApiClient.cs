2dc8ef16312651d2e7ceafa5205ae8a148f33d5e
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2025-07-03 19:33:50 +0900

    モデレータのログイン処理を実装

--- HJP-API-Client.NET/Client/HjpApiClient.cs
+++ HJP-API-Client.NET/Client/HjpApiClient.cs
@@ -2,6 +2,7 @@ using Hjp.Api.Client.Common;
 using Hjp.Api.Client.Dto;
 using Hjp.Api.Client.Interfaces;
 using Hjp.Api.Client.Internal;
+using Hjp.Shared.Dto.Moderator.Login;
 using Hjp.Shared.Dto.Users.Login;
 
 namespace Hjp.Api.Client
@@ -11,22 +12,25 @@ namespace Hjp.Api.Client
         private readonly ApiClientInternal apiClientInternal;
 
         private UsersClient? usersClient;
+        private ModeratorClient? moderatorClient;
 
         /// <summary>
         /// ユーザ関連APIのクライアント
         /// </summary>
         /// <remarks>先に <see cref="LoginWithUserAsync(string, CancellationToken)"/> を実行してください。それまでは null を返します。</remarks>
-        public IUsersClient UsersClient
-        {
-            get
-            {
-                return this.usersClient ?? throw new InvalidOperationException(Messages.Erros.NotLoggedInWithUser);
-            }
-        }
+        public IUsersClient UsersClient => this.usersClient ?? throw new InvalidOperationException(Messages.Erros.NotLoggedInWithUser);
         /// <summary>
         /// ユーザでログインしているか
         /// </summary>
         public bool IsLoggedInWithUser => this.usersClient != null;
+        /// <summary>
+        /// モデレータ関連APIのクライアント
+        /// </summary>
+        public IModeratorClient ModeratorClient => this.moderatorClient ?? throw new InvalidOperationException(Messages.Erros.NotLoggedInWithModerator);
+        /// <summary>
+        /// モデレータでログインしているか
+        /// </summary>
+        public bool IsLoggedInWithModerator => this.moderatorClient != null;
 
         /// <summary>
         /// コンストラクタ
@@ -65,6 +69,31 @@ namespace Hjp.Api.Client
         {
             var result = this.usersClient != null;
             this.usersClient = null;
+            return result;
+        }
+
+        /// <summary>
+        /// モデレータとしてログイン
+        /// </summary>
+        public async Task<ApiResponse<ModeratorLoginResponse>> LoginWithModeratorAsync(ModeratorLoginRequest request, CancellationToken cancellationToken = default)
+        {
+            var result = await this.apiClientInternal.PostAsync<ModeratorLoginResponse>("moderator/login", request, null, cancellationToken);
+            if (result.IsSuccess == true && result.Result != null)
+            {
+                this.moderatorClient = new(this.apiClientInternal, result.Result.DiscordUserId);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// モデレータからログアウト
+        /// </summary>
+        /// <returns>ログアウトに成功したか</returns>
+        public bool LogoutWithModerator()
+        {
+            var result = this.moderatorClient != null;
+            this.moderatorClient = null;
             return result;
         }
     }
--- HJP-API-Client.NET/Common/Messages.cs
+++ HJP-API-Client.NET/Common/Messages.cs
@@ -5,6 +5,7 @@ namespace Hjp.Api.Client.Common
         public static class Erros
         {
             public const string NotLoggedInWithUser = "先に LoginWithUserAsync を実行してください。";
+            public const string NotLoggedInWithModerator = "先に LoginWithModeratorAsync を実行してください。";
 
             public const string EmptyResponseBody = "Empty response body";
         }
