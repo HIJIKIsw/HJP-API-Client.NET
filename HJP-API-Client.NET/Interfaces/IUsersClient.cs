52f75fadf04345e90a503ce1cb604b9ebc54b235
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2025-07-05 03:35:02 +0900

    サマリコメントの誤りを修正

--- HJP-API-Client.NET/Interfaces/IUsersClient.cs
+++ HJP-API-Client.NET/Interfaces/IUsersClient.cs
@@ -14,21 +14,18 @@ namespace Hjp.Api.Client.Interfaces
         /// <summary>
         /// ユーザ情報を取得
         /// </summary>
-        /// <param name="discordUserId">DiscordユーザーID</param>
         /// <remarks>先に <see cref="HjpApiClient.LoginWithUserAsync"/> を実行してください</remarks>
         Task<ApiResponse<UserResponse>> GetProfileAsync(CancellationToken cancellationToken = default);
 
         /// <summary>
         /// ユーザの残高を取得
         /// </summary>
-        /// <param name="discordUserId">DiscordユーザーID</param>
         /// <remarks>先に <see cref="HjpApiClient.LoginWithUserAsync"/> を実行してください</remarks>
         Task<ApiResponse<UserBalanceResponse>> GetBalanceAsync(CancellationToken cancellationToken = default);
 
         /// <summary>
         /// ユーザの取引履歴を取得
         /// </summary>
-        /// <param name="discordUserId">DiscordユーザーID</param>
         /// <param name="query">取得条件 (省略時はフィルタしない/ 最大500件)</param>
         /// <remarks>先に <see cref="HjpApiClient.LoginWithUserAsync"/> を実行してください</remarks>
         Task<ApiResponse<UserTransactionsResponse>> GetTransactionsAsync(UserTransactionsRequest? query = null, CancellationToken cancellationToken = default);
@@ -36,14 +33,12 @@ namespace Hjp.Api.Client.Interfaces
         /// <summary>
         /// ユーザの統計情報を取得
         /// </summary>
-        /// <param name="discordUserId">DiscordユーザーID</param>
         /// <remarks>先に <see cref="HjpApiClient.LoginWithUserAsync"/> を実行してください</remarks>
         Task<ApiResponse<UserStatsResponse>> GetStatsAsync(CancellationToken cancellationToken = default);
 
         /// <summary>
         /// ユーザの入金
         /// </summary>
-        /// <param name="discordUserId">DiscordユーザーID</param>
         /// <param name="request">入金情報</param>
         /// <remarks>先に <see cref="HjpApiClient.LoginWithUserAsync"/> を実行してください</remarks>
         Task<ApiResponse<UserDepositResponse>> DepositAsync(UserDepositRequest request, CancellationToken cancellationToken = default);
@@ -51,7 +46,6 @@ namespace Hjp.Api.Client.Interfaces
         /// <summary>
         /// ユーザの出金
         /// </summary>
-        /// <param name="discordUserId">DiscordユーザーID</param>
         /// <param name="request">出金情報</param>
         /// <remarks>先に <see cref="HjpApiClient.LoginWithUserAsync"/> を実行してください</remarks>
         Task<ApiResponse<UserWithdrawResponse>> WithdrawAsync(UserWithdrawRequest request, CancellationToken cancellationToken = default);
@@ -59,7 +53,6 @@ namespace Hjp.Api.Client.Interfaces
         /// <summary>
         /// ユーザの送金
         /// </summary>
-        /// <param name="discordUserId">DiscordユーザーID</param>
         /// <param name="request">送金情報</param>
         /// <remarks>先に <see cref="HjpApiClient.LoginWithUserAsync"/> を実行してください</remarks>
         Task<ApiResponse<UserTransferResponse>> TransferAsync(UserTransferRequest request, CancellationToken cancellationToken = default);
