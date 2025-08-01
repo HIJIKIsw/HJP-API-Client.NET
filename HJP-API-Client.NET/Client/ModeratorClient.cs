efbb98c0c607babd9711876794c0ce0fed53d66a
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2025-08-02 05:34:49 +0900

    モデレータに追加されたAPIを実装

--- HJP-API-Client.NET/Client/ModeratorClient.cs
+++ HJP-API-Client.NET/Client/ModeratorClient.cs
@@ -1,8 +1,12 @@
 using Hjp.Api.Client.Dto;
 using Hjp.Api.Client.Interfaces;
 using Hjp.Api.Client.Internal;
+using Hjp.Shared.Dto.Moderator.Users;
 using Hjp.Shared.Dto.Moderator.Users.AccessToken.Reset;
+using Hjp.Shared.Dto.Moderator.Users.Lottery;
 using Hjp.Shared.Dto.Moderator.Users.Register;
+using Hjp.Shared.Dto.Moderator.Users.ServerActivityReward;
+using Hjp.Shared.Dto.Moderator.Users.Transfer;
 
 namespace Hjp.Api.Client
 {
@@ -12,6 +16,31 @@ namespace Hjp.Api.Client
         : base(apiClientInternal, signature)
         {
             // Nothind to do.
+        }
+
+        public async Task<ApiResponse<ModeratorUserBalanceResponse>> GetUserBalanceAsync(ulong discordUserId, CancellationToken cancellationToken = default)
+        {
+            await this.InvokeOnBeforeMethodAsync(cancellationToken);
+
+            return await this.apiClientInternal.GetWithSignatureAsync<ModeratorUserBalanceResponse>(
+                signature: this.signature,
+                route: $"moderator/users/{discordUserId}/balance",
+                query: null,
+                isIncludeNonce: true,
+                cancellationToken: cancellationToken);
+        }
+
+        public async Task<ApiResponse<ModeratorUserLotteryResponse>> DrawUserLotteryAsync(ulong discordUserId, ModeratorUserLotteryRequest request, CancellationToken cancellationToken = default)
+        {
+            await this.InvokeOnBeforeMethodAsync(cancellationToken);
+
+            return await this.apiClientInternal.PostWithSignatureAsync<ModeratorUserLotteryResponse>(
+                signature: this.signature,
+                route: $"moderator/users/{discordUserId}/lottery/draw",
+                body: request,
+                query: null,
+                isIncludeNonce: true,
+                cancellationToken: cancellationToken);
         }
 
         public async Task<ApiResponse<ModeratorUserRegisterResponse>> RegisterUserAsync(ModeratorUserRegisterRequest request, CancellationToken cancellationToken = default)
@@ -34,6 +63,32 @@ namespace Hjp.Api.Client
             return await this.apiClientInternal.PostWithSignatureAsync<ModeratorUserAccessTokenResetResponse>(
                 signature: this.signature,
                 route: $"moderator/users/{discordUserId}/access-token/reset",
+                body: request,
+                query: null,
+                isIncludeNonce: true,
+                cancellationToken: cancellationToken);
+        }
+
+        public async Task<ApiResponse<ModeratorServerActivityRewardResponse>> DepositServerActivityRewardAsync(ulong discordUserId, ModeratorServerActivityRewardRequest request, CancellationToken cancellationToken = default)
+        {
+            await this.InvokeOnBeforeMethodAsync(cancellationToken);
+
+            return await this.apiClientInternal.PostWithSignatureAsync<ModeratorServerActivityRewardResponse>(
+                signature: this.signature,
+                route: $"moderator/users/{discordUserId}/server-activity-reward",
+                body: request,
+                query: null,
+                isIncludeNonce: true,
+                cancellationToken: cancellationToken);
+        }
+
+        public async Task<ApiResponse<ModeratorUserTransferResponse>> UserTransferAsync(ulong discordUserId, ModeratorUserTransferRequest request, CancellationToken cancellationToken = default)
+        {
+            await this.InvokeOnBeforeMethodAsync(cancellationToken);
+
+            return await this.apiClientInternal.PostWithSignatureAsync<ModeratorUserTransferResponse>(
+                signature: this.signature,
+                route: $"moderator/users/{discordUserId}/transfer",
                 body: request,
                 query: null,
                 isIncludeNonce: true,
--- HJP-API-Client.NET/Interfaces/IModeratorClient.cs
+++ HJP-API-Client.NET/Interfaces/IModeratorClient.cs
@@ -1,11 +1,28 @@
 using Hjp.Api.Client.Dto;
+using Hjp.Shared.Dto.Moderator.Users;
 using Hjp.Shared.Dto.Moderator.Users.AccessToken.Reset;
+using Hjp.Shared.Dto.Moderator.Users.Lottery;
 using Hjp.Shared.Dto.Moderator.Users.Register;
+using Hjp.Shared.Dto.Moderator.Users.ServerActivityReward;
+using Hjp.Shared.Dto.Moderator.Users.Transfer;
 
 namespace Hjp.Api.Client.Interfaces
 {
     public interface IModeratorClient
     {
+        /// <summary>
+        /// ユーザの残高を取得
+        /// </summary>
+        /// <param name="discordUserId">残高を取得するユーザのDiscordユーザID</param>
+        Task<ApiResponse<ModeratorUserBalanceResponse>> GetUserBalanceAsync(ulong discordUserId, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// ユーザのくじを実行
+        /// </summary>
+        /// <param name="discordUserId">くじを実行するユーザのDiscordユーザID</param>
+        /// <param name="request">リクエスト詳細</param>
+        Task<ApiResponse<ModeratorUserLotteryResponse>> DrawUserLotteryAsync(ulong discordUserId, ModeratorUserLotteryRequest request, CancellationToken cancellationToken = default);
+
         /// <summary>
         /// ユーザ登録
         /// </summary>
@@ -18,5 +35,19 @@ namespace Hjp.Api.Client.Interfaces
         /// </summary>
         /// <param name="discordUserId">アクセストークンを生成するユーザのDiscordユーザID</param>
         Task<ApiResponse<ModeratorUserAccessTokenResetResponse>> ResetUserAccessTokenAsync(ulong discordUserId, ModeratorUserAccessTokenResetRequest request, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// サーバー活動報酬を入金
+        /// </summary>
+        /// <param name="discordUserId">入金するユーザのDiscordユーザID</param>
+        /// <param name="request">入金情報</param>
+        Task<ApiResponse<ModeratorServerActivityRewardResponse>> DepositServerActivityRewardAsync(ulong discordUserId, ModeratorServerActivityRewardRequest request, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// ユーザ間送金
+        /// </summary>
+        /// <param name="discordUserId">送金するユーザのDiscordユーザID</param>
+        /// <param name="request">送金情報</param>
+        Task<ApiResponse<ModeratorUserTransferResponse>> UserTransferAsync(ulong discordUserId, ModeratorUserTransferRequest request, CancellationToken cancellationToken = default);
     }
 }
